27368c66d7415258f6044ca0a028e9744d47eef1
Shotaro <s.kamada0918@gmail.com>
2022-08-03 17:12:24 +0900

    Setup Material UI

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import createEmotionCache from '../lib/createEmotionCache'
+import theme from '../lib/theme'
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache
+}
+
+function MyApp(props: MyAppProps) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+    return (
+        <Typography variant="h1">
+            Home
+        </Typography>
+    )
+}
+
+export default Home
